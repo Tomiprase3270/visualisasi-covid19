f003ed4a6d9a5696a4b0548f64798a3ac433b86f
Tomi Prasetyo <68488076+Tomiprase3270@users.noreply.github.com>
2021-10-19 11:49:33 +0800

    Update covid_visualization.py
    Add import xlrd

--- covid_visualization.py
+++ covid_visualization.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
-
+import xlrd
 
 # title
 st.title("Aplikasi Visualisasi Data Covid-19")
